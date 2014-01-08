52400929bad677cef775cb8d876294505663508c
Glenn <glennular@gmail.com>
2014-01-08 14:27:23 -0800

    Mongoose packages added

---
+++ mongoose.tiny/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try { 
+  $packageName = 'mongoose.tiny' 
+  $url = 'https://mongoose.googlecode.com/files/mongoose-5.0.exe'
+  $scriptPath = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+  $mongoosePath = Join-Path $scriptPath 'mongoose.tiny.exe'
+  
+  Get-ChocolateyWebFile "$packageName" "$mongoosePath" "$url"
+  
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
