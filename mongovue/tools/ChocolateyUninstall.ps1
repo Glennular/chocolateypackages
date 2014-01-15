0d3ebbf1f8d21485cd2ab280f5cf3a56bc280d51
glennular <glennular@gmail.com>
2014-01-15 08:54:14 -0600

    added mongoVue 1.6.6

---
+++ mongovue/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+﻿$package = 'MongoeVUE'
+
+try {
+  # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
+  $msiid = '{5B55CD4F-146D-4B7E-9E75-DE4432797A43}'
+  Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msIid /qb" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw 
+}
